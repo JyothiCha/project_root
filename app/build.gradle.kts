ca1d9dafef2df0ddb42c1dee69989c79085b80c6
JyothiCha <jyothichapati@gmail.com>
2025-04-22 04:22:32 +0000

    Bumped up version from 8.0.0 to 9.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         minSdkVersion(21)
         targetSdkVersion(30)
 
-        versionCode = 9  // Starting version code (to be incremented by your script)
-        versionName = "8.0.0"  // Starting version name (to be set by your script)
+        versionCode = 10  // Starting version code (to be incremented by your script)
+        versionName = "9.0.0"  // Starting version name (to be set by your script)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
