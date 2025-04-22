1a458dd79300f9052b94990c018271daa5f50d38
JyothiCha <jyothichapati@gmail.com>
2025-04-22 04:13:51 +0000

    Bumped up version from 4.0.0 to 3.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         minSdkVersion(21)
         targetSdkVersion(30)
 
-        versionCode = 4  // Starting version code (to be incremented by your script)
-        versionName = "4.0.0"  // Starting version name (to be set by your script)
+        versionCode = 5  // Starting version code (to be incremented by your script)
+        versionName = "3.0.0"  // Starting version name (to be set by your script)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
