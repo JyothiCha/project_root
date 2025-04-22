eb173438a60b27031aec87c97166b437bf044537
JyothiCha <jyothichapati@gmail.com>
2025-04-22 08:34:04 +0530

    Created build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(30)  // Specify the SDK version
+
+    defaultConfig {
+        applicationId = "com.example.myapp"
+        minSdkVersion(21)
+        targetSdkVersion(30)
+
+        versionCode = 1  // Starting version code (to be incremented by your script)
+        versionName = "1.0.0"  // Starting version name (to be set by your script)
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    // Other dependencies
+}
